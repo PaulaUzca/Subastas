9b8dc190f737199de89b2642432aeaad19bb1df2
PaulaUzca <paula.uzcategui68@gmail.com>
2022-08-16 19:47:05 -0500

    updated requirements.txt

--- commerce/settings.py
+++ commerce/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY= os.environ["SECRET_KEY"]
-
+#SECRET_KEY= os.environ["SECRET_KEY"]
+SECRET_KEY=  '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"]
+#DEBUG = os.environ["DEBUG"]
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
